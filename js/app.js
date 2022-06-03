262619f2787a3943c9cfc842b6fa2f4aeb63f239
Flabby-Owl <chaussee.jeremie@tutamail.com>
2022-06-03 16:17:08 +0200

    Réindentation + Changements Fonts + suppression de fichiers useless + redimenssionnement navbar

--- js/app.js
+++ js/app.js
@@ -1,27 +1,28 @@
 function startTime() {
-    var today = new Date();
-    var h = today.getHours();
-    var m = today.getMinutes();
-    m = checkTime(m);
-    document.getElementById('time').innerHTML =
-    +h + ":" + m;
-    var t = setTimeout(startTime, 500);
-  }
-  function checkTime(i) {
-    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
-    return i;
-  }
+  var today = new Date();
+  var h = today.getHours();
+  var m = today.getMinutes();
+  m = checkTime(m);
+  document.getElementById('time').innerHTML = +h + ":" + m;
+  var t = setTimeout(startTime, 500);
+}
 
-  function startDate() {
-    date = new Date();
-    day = date.getDate();
-    month = date.getMonth() + 1;
-    year = date.getFullYear();
+function checkTime(i) {
+  if (i < 10) {
+    i = "0" + i
+  };
+  return i;
+}
 
-   document.getElementById("date").innerHTML = day + "/" + month + "/" + year;      
-  }
+function startDate() {
+  date = new Date();
+  day = date.getDate();
+  month = date.getMonth() + 1;
+  year = date.getFullYear();
 
+  document.getElementById("date").innerHTML =
+    day + "/" + month + "/" + year;
+}
 
-  
-  startTime();
-  startDate();
+startTime();
+startDate();
